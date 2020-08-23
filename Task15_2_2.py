4824a5206a6b0e9b6033b9be779972d2ca6bfc6c
metafridays <metafridays@live.com>
2020-08-23 11:40:02 +0300

    modified:   Task15_2_2.py

--- Task15_2_2.py
+++ Task15_2_2.py
@@ -7,12 +7,6 @@ def descend_bubble(A):
             A[j], A[j+1] = A[j+1], A[j]
     return A
 
-N = 10
-a = []
-for i in range(N):
-    a.append(randint(1, 99))
 a = [2, 7, 3, 8, 78, 34, 67, 33, 8]
-print(a)
 descend_bubble(a)
-print(a)
 print(a[0]+a[5])
