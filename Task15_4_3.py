5af3cc9726d3f71016b7559cc583d997393c02b3
metafridays <36562671+metafridays@users.noreply.github.com>
2020-08-24 16:54:09 +0300

    Update Task15_4_3.py

--- Task15_4_3.py
+++ Task15_4_3.py
@@ -1,4 +1,4 @@
 list_to_sort = [25, 47, 13, 5, 77, 12]
 list_to_sort.sort(key = lambda x: str(x)[0])
-list_to_sort = [int(item) for item in list_to_sort]
-print("".join(str(list_to_sort).replace('[', '').replace(']', '')))
+list_to_sort = [str(item) for item in list_to_sort]
+print(", ".join(list_to_sort))
