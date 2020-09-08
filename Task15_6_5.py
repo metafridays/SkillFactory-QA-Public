21e002605d8a7a53d8b100d39c6db1beed333ef2
metafridays <metafridays@live.com>
2020-09-08 13:13:34 +0300

    Task15_6_5.py finished with lambda functions

--- Task15_6_5.py
+++ Task15_6_5.py
@@ -1,9 +1,5 @@
-def sort_key(k):
-    return k[0]
-
-
 a = [['Егор', 15, 165, 44], ['Лена', 20, 160, 45], ['Витя', 32, 180, 77], ['Полина', 28, 175, 65]]
 
-a_sort = sorted(a, key=sort_key)
+a_sort = sorted(a, key=lambda x: x[0])
 print(a)
 print(a_sort)
